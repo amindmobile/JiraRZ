f8e35e25a007802f851814ccc30a16a4faecdd8a
Aleksandr <amindmobile@gmail.com>
2020-11-26 20:27:29 +0300

    Branch testing

--- barsik.py
+++ barsik.py
@@ -9,24 +9,29 @@
 # print(barsik.talk())
 
 
-import numpy as np
-import pandas as pd
-from settings import all_issues, jira_atl, project, jira2
-from ast import literal_eval
+# import numpy as np
+# import pandas as pd
+# from settings import all_issues, jira_atl, project, jira2
+# from ast import literal_eval
+#
+#
+# # df = pd.read_csv('tasks1.csv', names=['Key', 'Status', 'Type', 'Link'])
+# # df = pd.read_csv('tasks1.csv', names=['Key', 'Status', 'Type', 'Summary', 'Link'])
+# df = pd.read_csv('tasks1.csv')
+# print(df['Link'])
+# df['Link'] = df['Link'].apply(literal_eval)
+#
+#
+# def check_link(link):
+#     return not bool(int(link[-1]) % 2)
+#
+#
+# def notify(code):
+#     print(f'{code} is done')
 
 
-# df = pd.read_csv('tasks1.csv', names=['Key', 'Status', 'Type', 'Link'])
-# df = pd.read_csv('tasks1.csv', names=['Key', 'Status', 'Type', 'Summary', 'Link'])
-df = pd.read_csv('tasks1.csv')
-print(df['Link'])
-df['Link'] = df['Link'].apply(literal_eval)
+from math import sin, cos, tan
 
-
-def check_link(link):
-    return not bool(int(link[-1]) % 2)
-
-
-def notify(code):
-    print(f'{code} is done')
-
+x = 100500
+print(sin(x) + cos(x) + tan(x))
 
--- main.py
+++ main.py
@@ -3,25 +3,57 @@ import pandas as pd
 from ast import literal_eval
 from settings import jira2, all_issues
 from time import perf_counter
+import multiprocessing as mp
+import timeit
+
 
 # rz = Rz()
+# processes = []
+# start = perf_counter()
 
-start = perf_counter()
+# Вспомогательная функция-шаблон для отправки уведомлений о полностью завершенной User Story
+def notify(task_id):
+    print(f'{task_id} is done.')
 
 
+# Read CSV to dada frame. Trying to parse list in list in df 'Link' column
 def issues_csv_filter():
     df = pd.read_csv('tasks.csv')
-    df = df[(df['Status'] == 'ToDo') & (df['Type'] == 'User Story') & (df['Link'] != '[]')]
-    df['Link'] = df.Link.apply(literal_eval)
-    df.to_csv('tasks1.csv', index=False)
+    df = df[(df['Status'] == 'ToDo') & (df['Type'] == 'User Story') & (df['Link'] != '[]') & df.Link.apply(literal_eval)]
+    pd.concat([df, pd.DataFrame(df.Link.tolist())], 1)
+    print(df)
+
+    issues_csv_filter()
+
+    # for id_ in df['Link']:
+    #     for j in list(id_):
+    #         print(j)
+
+    # for string_ in df.Link.apply(literal_eval):
+    #     for task_id in string_:
+    #         print(task_id)
+
+# Распечатать строки+столбцы на выбор
+# for index, row in df.iterrows():
+#     print(row['Status'], row['Link'])
 
 
-issues_csv_filter()
-finish = perf_counter()
-print(f'Finished in {round(finish - start, 2)} second(s)')
+# df.to_csv('tasks1.csv', index=False)
 
 
-#rz.issues_csv_filter()
+# for _ in range(8):
+#     p = mp.Process(target=issues_csv_filter)
+#     p.start()
+#     processes.append(p)
+#
+# for process in processes:
+#     process.join()
+
+# finish = perf_counter()
+# print(f'Finished in {round(finish - start, 2)} second(s)')
+
+
+# rz.issues_csv_filter()
 
 
 # df = pd.read_csv('tasks1.csv')
@@ -34,8 +66,8 @@ print(f'Finished in {round(finish - start, 2)} second(s)')
 #         print(i)
 
 
-    # поработать с этим!
-    # df['tasks1.csv'] = df[строка].apply(literal_eval)
+# поработать с этим!
+# df['tasks1.csv'] = df[строка].apply(literal_eval)
 #     print(df[df["Link"] == "[]"])
 #    return df[df("Status") == "ToDo" and df("Type") == "User Story"]
 #    return type(df.loc[8, df['Link']])
